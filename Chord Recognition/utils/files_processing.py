ade92ddcdff702ed5e940559d52a8673f76f491c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-08 16:22:27 +0000

    Adding tarfile member sanitization to extractall()

--- Chord Recognition/utils/files_processing.py
+++ Chord Recognition/utils/files_processing.py
@@ -10,7 +10,26 @@ def download_uncompress_data_to_local(result_folder, file_url=standard_file_url)
     os.mkdir(result_folder)
     urllib.request.urlretrieve(file_url, local_file_name)
     with tarfile.open(local_file_name, "r:gz") as f:
-        f.extractall('Beatles Annotations')
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(f, "Beatles Annotations")
 
 def filter_lab_files(search_folder, result_folder):
     folders_list_path = f'{search_folder}/chordlab/The Beatles'
